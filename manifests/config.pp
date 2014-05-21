03c39f2ca02dd7472c0dc55718efd0bfc76481da
Pascal Jufer <pascal.jufer@stepping-stone.ch>
2014-05-21 17:48:01 +0200

    add debian configuration

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,6 +11,32 @@ class alfresco::config {
 
   case $::osfamily {
     debian: {
+      # the configuration files
+      file { "alfresco-global.properties":
+        path => "${alfresco_dir}/tomcat/shared/classes/alfresco-global.properties",
+        content => template("alfresco/alfresco-global.properties.erb"),
+      }
+
+      file { "${alfresco_dir}/tomcat/shared/classes/alfresco":
+        ensure => directory,
+        owner => $user,
+        group => $user,
+        mode => 0755,
+      }
+
+      file { "${alfresco_dir}/tomcat/shared/classes/alfresco/web-extension":
+        ensure => directory,
+        owner => $user,
+        group => $user,
+        mode => 0755,
+        require => File["${alfresco_dir}/tomcat/shared/classes/alfresco"],
+      }
+
+      file { "share-config-custom.xml":
+        path => "${alfresco_dir}/tomcat/shared/classes/alfresco/web-extension/share-config-custom.xml",
+        content => template("alfresco/share-config-custom.xml.erb"),
+        require => File["${alfresco_dir}/tomcat/shared/classes/alfresco/web-extension"],
+      }
     }
     gentoo: {
     }
