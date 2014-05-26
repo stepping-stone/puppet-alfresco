4edfb2340f36f723cc6c0f8cdd0f0defa4687de3
Pascal Jufer <pascal.jufer@stepping-stone.ch>
2014-05-26 17:42:11 +0200

    test

--- manifests/init.pp
+++ manifests/init.pp
@@ -92,6 +92,8 @@ class alfresco (
   $custom_settings           = $alfresco::params::custom_settings,
 ) inherits alfresco::params {
 
+  include apache::params
+
   case $::osfamily {
     debian: {
       $database_user_debian     = $database_user
